9c2ce38538df47480b49e560c4fa8a0403f032d4
GuengdongKim <guengdong@gmail.com>
2020-11-26 14:27:25 +0900

    Add files via upload

---
+++ ControlHealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ControlHealthBar : MonoBehaviour
+{
+    public Slider HealthBar;
+    public float damageHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space)) //체력 감소 
+        {
+            HealthBar.value -= damageHealth;
+        }
+        
+    }
+}
