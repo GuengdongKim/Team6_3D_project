c11d5b1f1c1a8f15006fc94edff2aed562f5314a
Luran-Han <sewong00@naver.com>
2020-12-01 15:38:41 +0900

    DoorController

---
+++ DoorController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorController : MonoBehaviour
+{
+    public Text text;
+	void Start()
+    {
+        text.enabled = false;
+
+    }
+
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Door")
+        {
+            text.enabled = true;
+            Animator anim = other.GetComponentInChildren<Animator>();
+            if(Input.GetKeyDown(KeyCode.E))
+                anim.SetTrigger("OpenClose");
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Door")
+        {
+            text.enabled = false;
+        }
+    }
+}
